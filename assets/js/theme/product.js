f49290a7cc120d3a30c20969692b66786a352535
quintin <qdhenry@gmail.com>
2020-09-14 14:48:55 -0400

    Refactor: style cleaenup

--- assets/js/theme/product.js
+++ assets/js/theme/product.js
@@ -7,9 +7,7 @@ import collapsibleFactory from './common/collapsible';
 import ProductDetails from './common/product-details';
 import videoGallery from './product/video-gallery';
 import { classifyForm } from './common/utils/form-utils';
-import utils from '@bigcommerce/stencil-utils';
-import axios from 'axios';
-import cartChecker from './customCartChecker.js';
+import cartChecker from './customCartChecker';
 
 
 export default class Product extends PageManager {
@@ -34,28 +32,6 @@ export default class Product extends PageManager {
         If true, then add a different product to the cart.
         */
         cartChecker();
-        // utils.api.cart.getCart({}, (err, response) => {
-        //     // if no cart items present or err return
-        //     if (err || !response) return;
-        //     // get the current contents of the shopping cart
-        //     const productData = response.lineItems.physicalItems;
-        //     // extract just the sku number from the products currently in the cart
-        //     const productIdsInCart = productData.map(p => p.sku);
-        //     // check what product is currently being viewed in the storefront
-        //     const currentProductBeingViewed = window.BCData.product_attributes.sku;
-        //     // check if the product being viewed matches
-        //     // any of the products present in shopping cart
-        //     const isProductCurrentlyInCart = productIdsInCart.filter(p => currentProductBeingViewed === p);
-        //     if (isProductCurrentlyInCart.length) {
-        //         return 'product already in cart, no update required';
-        //     }
-        //     // add item being viewed into user shopping cart
-        //     axios.get(`/cart.php?action=add&sku=${window.BCData.product_attributes.sku}`).then(() => {
-        //         alert('PRODUCT SUCCESSFULLY ADDED TO CART');
-        //     }).catch(e => {
-        //         throw new Error('error occured while adding item to cart', e);
-        //     });
-        // });
 
         let validator;
 
